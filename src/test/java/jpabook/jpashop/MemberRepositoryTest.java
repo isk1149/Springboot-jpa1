b9b6720ea76834a48d0823ee0b3d282ef2c2c18e
Inseong Kim <isk1149@naver.com>
2023-01-31 22:19:21 +0900

    엔티티 구성

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+//import static org.junit.Assert.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class) //junit에게 스프링과 관련된 테스트를 할 것이라 알려줌
+@SpringBootTest
+class MemberRepositoryTest {
+
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional //test 끝나면 롤백
+//    @Rollback(value = false) //테스트가 끝나도 데이터가 남아있게 함
+//    public void testMember() throws Exception {
+//        //given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        //when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//    }
+
+}
